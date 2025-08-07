ccf2f99d15cf90707dde26a042b7d78491916643
Lorena Coutinho <lorenalessa90@gmail.com>
2025-08-07 19:50:53 +0000

    Failed to load resource: the server responded with a status of 403 ()Ent

--- src/components/dashboard/expense-chart.tsx
+++ src/components/dashboard/expense-chart.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Pie, PieChart, ResponsiveContainer, Tooltip, Cell, Legend } from "recharts"
+import { Pie, PieChart, Tooltip, Cell, ResponsiveContainer } from "recharts"
 
 import {
   ChartConfig,
@@ -51,38 +51,36 @@ export default function ExpenseChart() {
   }, [])
 
   return (
-    <ChartContainer
-      config={chartConfig}
-      className="mx-auto aspect-square max-h-[250px]"
-    >
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart>
-          <Tooltip
-            cursor={false}
-            content={<ChartTooltipContent
-              hideLabel
-              formatter={(value) => `R$ ${Number(value).toFixed(2).replace('.',',')}`}
-            />}
-          />
-          <Pie
-            data={chartData}
-            dataKey="expenses"
-            nameKey="category"
-            innerRadius="60%"
-            strokeWidth={2}
-          >
-             <Cell key="cell-0" fill={chartConfig.Alimentação.color} />
-             <Cell key="cell-1" fill={chartConfig.Transporte.color} />
-             <Cell key="cell-2" fill={chartConfig.Moradia.color} />
-             <Cell key="cell-3" fill={chartConfig.Lazer.color} />
-             <Cell key="cell-4" fill={chartConfig.Outros.color} />
-          </Pie>
-           <ChartLegend
-            content={<ChartLegendContent nameKey="category" />}
-            className="-translate-y-[2px] flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
-          />
-        </PieChart>
-      </ResponsiveContainer>
-    </ChartContainer>
+    <div className="mx-auto aspect-square max-h-[250px] h-[250px] w-[250px]">
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Tooltip
+              cursor={false}
+              content={<ChartTooltipContent
+                hideLabel
+                config={chartConfig}
+                formatter={(value) => `R$ ${Number(value).toFixed(2).replace('.',',')}`}
+              />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="expenses"
+              nameKey="category"
+              innerRadius="60%"
+              strokeWidth={2}
+            >
+               <Cell key="cell-0" fill={chartConfig.Alimentação.color} />
+               <Cell key="cell-1" fill={chartConfig.Transporte.color} />
+               <Cell key="cell-2" fill={chartConfig.Moradia.color} />
+               <Cell key="cell-3" fill={chartConfig.Lazer.color} />
+               <Cell key="cell-4" fill={chartConfig.Outros.color} />
+            </Pie>
+             <ChartLegend
+              content={<ChartLegendContent nameKey="category" />}
+              className="-translate-y-[2px] flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
+            />
+          </PieChart>
+        </ResponsiveContainer>
+    </div>
   )
 }
