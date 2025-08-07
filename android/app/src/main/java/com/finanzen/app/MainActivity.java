dbc5de137a6cc7549cd9f8a74bccf3804fef55e5
Lorena Coutinho <lorenalessa90@gmail.com>
2025-08-07 23:11:42 +0000

    erro no teste do apk de http://localhost:3000/

---
+++ android/app/src/main/java/com/finanzen/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.finanzen.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.finanzen.app',
+  appName: 'FinanZen',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
