13401a3217ffb57cb5ecf39ad57e490c2c15bc7e
Lorena Coutinho <lorenalessa90@gmail.com>
2025-08-07 19:51:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dashboard/expense-chart.tsx
+++ src/components/dashboard/expense-chart.tsx
@@ -51,36 +51,39 @@ export default function ExpenseChart() {
   }, [])
 
   return (
-    <div className="mx-auto aspect-square max-h-[250px] h-[250px] w-[250px]">
-        <ResponsiveContainer width="100%" height="100%">
-          <PieChart>
-            <Tooltip
-              cursor={false}
-              content={<ChartTooltipContent
-                hideLabel
-                config={chartConfig}
-                formatter={(value) => `R$ ${Number(value).toFixed(2).replace('.',',')}`}
-              />}
-            />
-            <Pie
-              data={chartData}
-              dataKey="expenses"
-              nameKey="category"
-              innerRadius="60%"
-              strokeWidth={2}
-            >
-               <Cell key="cell-0" fill={chartConfig.Alimentação.color} />
-               <Cell key="cell-1" fill={chartConfig.Transporte.color} />
-               <Cell key="cell-2" fill={chartConfig.Moradia.color} />
-               <Cell key="cell-3" fill={chartConfig.Lazer.color} />
-               <Cell key="cell-4" fill={chartConfig.Outros.color} />
-            </Pie>
-             <ChartLegend
-              content={<ChartLegendContent nameKey="category" />}
-              className="-translate-y-[2px] flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
-            />
-          </PieChart>
-        </ResponsiveContainer>
-    </div>
+    <ChartContainer
+      config={chartConfig}
+      className="mx-auto aspect-square max-h-[250px]"
+    >
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart>
+          <Tooltip
+            cursor={false}
+            content={<ChartTooltipContent
+              hideLabel
+              config={chartConfig}
+              formatter={(value) => `R$ ${Number(value).toFixed(2).replace('.',',')}`}
+            />}
+          />
+          <Pie
+            data={chartData}
+            dataKey="expenses"
+            nameKey="category"
+            innerRadius="60%"
+            strokeWidth={2}
+          >
+             <Cell key="cell-0" fill={chartConfig.Alimentação.color} />
+             <Cell key="cell-1" fill={chartConfig.Transporte.color} />
+             <Cell key="cell-2" fill={chartConfig.Moradia.color} />
+             <Cell key="cell-3" fill={chartConfig.Lazer.color} />
+             <Cell key="cell-4" fill={chartConfig.Outros.color} />
+          </Pie>
+           <ChartLegend
+            content={<ChartLegendContent nameKey="category" />}
+            className="-translate-y-[2px] flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartContainer>
   )
 }
