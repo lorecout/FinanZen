bdadf3f282365e7ded44430b24e275ca14fb8092
Lorena Coutinho <lorenalessa90@gmail.com>
2025-08-09 22:03:33 +0000

    Build file 'C:\Users\lore-\AndroidStudioProjects\FinanZen\android\capaci

--- android/app/src/main/java/com/finanzen/app/MainActivity.java
+++ android/app/src/main/java/com/finanzen/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.finanzen.app;
 
+import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Handle the splash screen transition.
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
+        super.onCreate(savedInstanceState);
+    }
+}
