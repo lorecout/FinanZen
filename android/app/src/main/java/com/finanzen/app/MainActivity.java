5776068d0b092b1c1fb6e9715862e64a462677b1
Lorena Coutinho <lorenalessa90@gmail.com>
2025-08-09 00:44:26 +0000

    Executing tasks: [:app:assembleDebug, :app:assembleDebugUnitTest, :app:a

--- android/app/src/main/java/com/finanzen/app/MainActivity.java
+++ android/app/src/main/java/com/finanzen/app/MainActivity.java
@@ -1,15 +1,5 @@
 package com.finanzen.app;
 
-import android.os.Bundle;
-import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // Handle the splash screen transition.
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
-        
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
