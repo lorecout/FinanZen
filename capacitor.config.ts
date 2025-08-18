d3860462616178e93d8ba18bb466a371e2b22b50
Lorena Coutinho <lorenalessa90@gmail.com>
2025-08-18 18:00:39 +0000

    o que faço pra seguir com a publicação?

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,6 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.finanzen.app',
   appName: 'FinanZen',
-  server: {
-    url: 'http://localhost:3000',
-    cleartext: true,
-  },
 };
 
 export default config;
